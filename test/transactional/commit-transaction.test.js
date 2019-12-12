0c1b15863857c756b50c80a8633ee3a862b3cc2d
Dave Preston <drpresto@yahoo.com>
2019-12-12 14:16:50 -0800

    feat: update mssql dependency to latest

--- test/transactional/commit-transaction.test.js
+++ test/transactional/commit-transaction.test.js
@@ -91,12 +91,12 @@ describe('Transactional ::', function () {
     // * Run a query on connectionB and make sure the record doesn't exist
     // * Commit the transaction
     // * Run the select query again and the record should exist
-    it('should perform a transaction and make sure the results are commited correctly', function (done) {
+    it.only('should perform a transaction and make sure the results are commited correctly', function (done) {
       // Start a transaction on connection A
       Pack.beginTransaction({
         connection: connectionA,
         meta: {
-          isolationLevel: 'READ_UNCOMMITED',
+          isolationLevel: 'READ_UNCOMMITTED',
         }
       })
         .exec(function (err) {
